d8458fc925495fc7390e0250197146d73ab583ed
adidier <adidier.perez@gmail.com>
2021-02-23 00:40:56 -0600

    Encription

--- Encryption/Encryption.cpp
+++ Encryption/Encryption.cpp
@@ -3,24 +3,44 @@
 
 using namespace std;
 
-string removeSpaces(string& s)
+
+std::string removeSpaces(std::string& s)
 {
-    auto found = s.begin();
-    while (found != s.end())
+    std::size_t found = s.find(" ");;
+    while (found != std::string::npos)
     {
-       found = s.find(" ");
-       if (found != s.end())
-       s.erase(found)
+        found = s.find(" ");
+        if (found != std::string::npos)
+            s.erase(s.begin() + found);
     }
+
+    return s;
+}
+
+std::string getSubstringAtIndex(const int& i, const std::string& s)
+{
+    return (i < s.size()) ? s.substr(i, 1) : std::string("");
 }
 
 // Complete the encryption function below.
-string encryption(string s)
+std::string encryption(std::string s)
 {
+    s = removeSpaces(s);
     float raiz = sqrt(s.length());
-    unsigned int row = floor(raiz);
-    unsigned int column = ceil(raiz);
-    
+    unsigned int columns = ceil(raiz);
+    unsigned int rows = floor(raiz) + (unsigned int)(((s.length() % columns) > 0) ? 1 : 0);
+
+    std::string result;
+    for (int i = 0; i < columns; ++i)
+    {
+        for (int j = 0; j < rows; ++j)
+        {
+            result.append(getSubstringAtIndex(j * columns + i, s));
+        }
+        result.append(" ");
+    }
+
+    return result;
 }
 
 int main()
